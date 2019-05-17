d63535fef1b5cea19b41290e3e39fa42b6060e65
lafarnelli <36866888+lafarnelli@users.noreply.github.com>
2019-05-16 21:48:00 -0500

    Update Collectionizing.R

--- Collectionizing.R
+++ Collectionizing.R
@@ -12,8 +12,6 @@
 #install.packages("tidyverse")
 #install.packages("stringr")
 #install.packages("doParallel")
-library(doParallel) 
-library(foreach)
 library(dplyr)
 library(tidyr)
 library(tidytext)
@@ -24,29 +22,29 @@ library(tidyverse)
 library(stringr)
 
 collections <- c("Visualizing Abolition and Freedom",
-"Social Gospel",
-"Scholarship at Grinnell",
-"Student Scholarship",
-"Faculty Scholarship",
-"Syllabi and Curricular Materials",
-"Early College History",
-"G.W. Cook Correspondence",
-"Grinnell College Buildings",
-"Historic Iowa Postcards",
-"Jimmy Ley Collection",
-"Kleinschmidt Architectural History",
-"Life at Grinnell College",
-"Social Gospel",
-"Grinnell College Geology Collection",
-"Visualizing Abolition and Freedom",
-"Ancient Coins",
-"Social Justice at Grinnell",
-"Poweshiek History Preservation Project")
+                 "Social Gospel",
+                 "Scholarship at Grinnell",
+                 "Student Scholarship",
+                 "Faculty Scholarship",
+                 "Syllabi and Curricular Materials",
+                 "Early College History",
+                 "G.W. Cook Correspondence",
+                 "Grinnell College Buildings",
+                 "Historic Iowa Postcards",
+                 "Jimmy Ley Collection",
+                 "Kleinschmidt Architectural History",
+                 "Life at Grinnell College",
+                 "Social Gospel",
+                 "Grinnell College Geology Collection",
+                 "Visualizing Abolition and Freedom",
+                 "Ancient Coins",
+                 "Social Justice at Grinnell",
+                 "Poweshiek History Preservation Project")
 
 colldf <- data.frame()
-colldf$name <- collections
-for i in collections{
-
-#Determine the number of objects in each collections
-colldf$freq <- length(grep(metadata$item, collections(i)))
+for (i in collections){
+  colldf$name[1] <- (collections[1])
+  
+  #Determine the number of objects in each collections
+  colldf$freq <- length(grep(metadata$item, collections(i)))
 }
